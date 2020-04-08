18f236be424664be436ad7d9dd6b74634f51bedd
Cindy Quach <cindyquach@csu.fullerton.edu>
2020-04-07 23:00:07 -0700

    nav bar pre-style

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -2,7 +2,46 @@
   <div>
     <h2>Nav Bar</h2>
     <nav>
-      <router-link to="/home">Home</router-link>
+			<router-link v-for="routes in links" 
+      v-bind:key="routes.id"
+      :to="`${routes.page}`">{{routes.text}}</router-link>
     </nav>
   </div>
 </template>
+
+<script>
+export default {
+  data() { 
+		return { 
+			links: [ 
+				{ 
+					id: 0,
+					text: 'Index', 
+					page: '/Index' 
+				},
+        {
+          id: 1,
+          text: 'Home',
+          page:'/Home'
+        },
+        {
+          id: 2,
+          text: 'About',
+          page:'/About'
+        },
+        {
+          id: 3,
+          text: 'Contact',
+          page:'/Contact'
+        } 
+			] 
+		} 
+	}
+}
+</script>
+
+<style> 
+  .spacing { 
+    Margin-right: 10px; 
+  } 
+</style>
